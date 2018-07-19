039b64fcfdd818e6e0753f154952ec014fc1edf8
ahuigo <a132811@gmail.com>
2018-07-19 20:23:09 +0800

    re package

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
-import re
-version = "0.1.0"
+import os,re
+version = "0.1.11"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='ahui_aiohttp_server',
@@ -13,17 +16,12 @@ setup(
     entry_points = {
         #'console_scripts': ['aiohttp_server=pyhttp_server:main', ],
     },
-
-    description = "Simple aiohttp Server for both static and python file",
-    long_description=open('README.md').read(),
+    description = "Simple aiohttp Server for both static and python/php file",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
     author = "ahuigo",
     author_email = "nobody@qq.com",
     license = "MIT",
     url = "http://github.com/ahuigo/ahui-aiohttp-server",   
 )
 
-s = open('setup.py').read()
-s = re.sub(r'(?<=\n)(version *= *")(\d+\.\d+\.)(\d+)"', 
-    lambda m: 'version = "%s%d"' % (m.group(2), int(m.group(3))+1),
-    s,1)
-open('setup.py', 'w').write(s)
